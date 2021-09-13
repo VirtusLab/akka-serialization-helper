7cccbc4099b6cea06295fd26ab7806272b99aa3e
MarconZet <marcin.zlakowski@gmail.com>
2021-09-13 13:51:29 +0200

    Bumped scala, sbt, sbt plugins and libraries versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,16 +1,15 @@
 import sbt._
 
 object Dependencies {
-  val scalaVersion213 = "2.13.5"
-  val scalaVersion212 = "2.12.13"
+  val scalaVersion213 = "2.13.6"
+  val scalaVersion212 = "2.12.14"
 
-  val akkaVersion = "2.6.13"
+  val akkaVersion = "2.6.16"
   val borerVersion = "1.7.2"
   val circeVersion = "0.14.1"
 
-  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.2"
-  val logger = "org.slf4j" % "slf4j-simple" % "1.7.30"
-  val enumeratum = "com.beachape" %% "enumeratum" % "1.6.1"
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.9"
+  val logger = "org.slf4j" % "slf4j-simple" % "1.7.32"
   val reflections = "net.oneandone.reflections8" % "reflections8" % "0.11.7"
   val betterFiles = "com.github.pathikrit" %% "better-files" % "3.9.1"
   val sprayJson = "io.spray" %% "spray-json" % "1.3.6"
@@ -19,7 +18,7 @@ object Dependencies {
   val akkaTyped = "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion
   val akkaStream = "com.typesafe.akka" %% "akka-stream" % akkaVersion
   val akkaPersistence = "com.typesafe.akka" %% "akka-persistence-typed" % akkaVersion
-  val akkaProjections = "com.lightbend.akka" %% "akka-projection-eventsourced" % "1.2.0"
+  val akkaProjections = "com.lightbend.akka" %% "akka-projection-eventsourced" % "1.2.2"
   val akkaTestKit = "com.typesafe.akka" %% "akka-actor-testkit-typed" % akkaVersion
   val akkaStreamTestKit = "com.typesafe.akka" %% "akka-stream-testkit" % akkaVersion
 
