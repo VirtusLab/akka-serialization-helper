06ef1524d14af839fd09e7222b0e6a24a4b7043b
Scala Steward <scala_steward@virtuslab.com>
2022-08-12 20:09:33 +0000

    Reformat with scalafmt 3.5.9
    Executed command: scalafmt --non-interactive

--- serializability-checker-compiler-plugin/src/test/resources/GenericsTest3.scala
+++ serializability-checker-compiler-plugin/src/test/resources/GenericsTest3.scala
@@ -8,8 +8,8 @@ import scala.concurrent.Future
 
 object GenericsTest3 {
 
-  def invokeGrpcWebHandler(handlers: List[PartialFunction[HttpRequest, Future[HttpResponse]]])
-                                  (implicit as: ActorSystem[_]): HttpRequest => Future[HttpResponse] = {
+  def invokeGrpcWebHandler(handlers: List[PartialFunction[HttpRequest, Future[HttpResponse]]])(implicit
+      as: ActorSystem[_]): HttpRequest => Future[HttpResponse] = {
     // For some reason type parameter on below method call for implicitly passed ActorSystem[_] is not `scala.Any`
     // but `org.random.project.GenericsTest3._$1`. That's why we use `WebHandler.grpcWebHandler` method in test.
     WebHandler.grpcWebHandler(handlers: _*)
