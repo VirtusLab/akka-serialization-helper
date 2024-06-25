6b3be9ef0e4251cb8ab1755b7d7b2b5d861a3979
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-06-25 15:34:21 +0200

    Update deps (#412)
    * Scala-Steward: Update circe-core from 0.14.7 to 0.14.8
    
    * Scala-Steward: Update scalatest from 3.2.18 to 3.2.19

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,10 +10,10 @@ object Dependencies {
   val akkaVersion = "2.6.20"
   val borerVersion = "1.8.0"
   val circeYamlVersion = "1.15.0"
-  val circeVersion = "0.14.7"
+  val circeVersion = "0.14.8"
   val circeGenericExtrasVersion = "0.14.3"
 
-  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.18"
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19"
   val logger = "org.slf4j" % "slf4j-simple" % "1.7.36"
   val reflections = "net.oneandone.reflections8" % "reflections8" % "0.11.7"
   val betterFiles = "com.github.pathikrit" %% "better-files" % "3.9.2"
